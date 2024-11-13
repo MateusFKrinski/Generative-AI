53a66327240119df747cdeeb087300bf242d519e
Mateus Francisco Krinski <mateus.krinski10@gmail.com>
2024-11-13 19:16:32 -0300

    Projeto IA

--- src/config/OpenAi.ts
+++ src/config/OpenAi.ts
@@ -1,7 +1,6 @@
 import OpenAI from 'openai';
 
-const API_KEY =
-  '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';
+const API_KEY = process.env.API_KEY;
 
 export const openai = new OpenAI({
   apiKey: API_KEY,
